2ffce6a89429d7500df4c7695bb0dacc979958da
NGUYEN QA <59968046+jyanqa@users.noreply.github.com>
2021-01-26 07:55:47 +0100

    Add files via upload

--- 947097/Run.py
+++ 947097/Run.py
@@ -10,8 +10,6 @@ from Import_Data import load_data
 import LinearRegMLE as lr
 import bootstrap as bt
 from clusterize import kmeans_cluster, classification_measure
-
-#from plota import scatterplot_with_colors
 #---------------------------------------------    
 def LRMLE():
     #load data
@@ -19,8 +17,8 @@ def LRMLE():
     #fit MLE Regresssion model
     lrmodel = lr.fitLR(dfy,dfx)
     print (lrmodel.summary())
-if __name__ == "__LRMLE__":
-    LRMLE()
+#if __name__ == "__LRMLE__":
+    #LRMLE()
  #---------------------------------------------       
 def l1():
     #load data
@@ -29,8 +27,8 @@ def l1():
     sumofloss = lr.l1(dfy,dfx)
     print(sumofloss.popularity)
     
-if __name__ == "__l1__":
-    l1()
+#if __name__ == "__l1__":
+   # l1()
 #---------------------------------------------    
 def bootme():
     #load
@@ -39,8 +37,8 @@ def bootme():
     coefboot = bt.boot(dfy,dfx)
     print(coefboot )
 
-if __name__ == "__bootme__":
-    bootme()
+#if __name__ == "__bootme__":
+   # bootme()
     
 def plotCI():
     #load
@@ -49,19 +47,24 @@ def plotCI():
     coefboot = bt.boot(dfy,dfx)
     print(coefboot )
 
-if __name__ == "__plotCI__":
-    plotCI()
+#if __name__ == "__plotCI__":
+    #plotCI()
 #--------------------------------------------- 
 def clust():
         true_labels, df = load_data(s.dataset) 
         pred_labels = kmeans_cluster(df, s.n_components)
         classification_measure(true_labels, pred_labels)
-if __name__ == "__clust__":
-    clust()    
+#if __name__ == "__clust__":
+    #clust()    
     
-    
-    
-    
+#__________________________________________________________   
+
+if __name__ == "__main__":
+    LRMLE()
+    l1()
+    bootme()
+    plotCI()
+    clust()
     
     
     
